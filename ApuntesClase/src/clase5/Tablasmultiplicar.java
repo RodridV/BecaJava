2de4d7a7cf156403486bad8836eb33954c43d231
rodri <rodri@DESKTOP-IU8B4JD>
2021-09-22 18:28:28 +0200

    Actualización tablas

--- ApuntesClase/src/clase5/Tablasmultiplicar.java
+++ ApuntesClase/src/clase5/Tablasmultiplicar.java
@@ -5,7 +5,7 @@ public class Tablasmultiplicar {
 	public static void main(String[] args) {
 		//Mostrar las 10 primeras tablas de multipicar y la suma de la tabla
 		
-		int multipl,suma=0;
+		int multipl,suma=0;				//Valor de cada multiplicación y suma acumulativa para cada tabla
 		for (int i=1;i<=10;i++) {		//Tablas del 1 al 10
 			System.out.println("Tabla del "+ i +":");
 			for(int j=1;j<=10;j++) {
