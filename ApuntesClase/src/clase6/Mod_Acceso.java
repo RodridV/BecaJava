fdc1701e1d0691a3bafe6a73fcc6f85fe3dd3f35
rodri <rodri@DESKTOP-IU8B4JD>
2021-09-23 14:00:57 +0200

    Campos privados y públicos

---
+++ ApuntesClase/src/clase6/Mod_Acceso.java
@@ -0,0 +1,15 @@
+package clase6;
+
+public class Mod_Acceso { 		//Modificadores de Acceso; asociado a Vacaciones
+
+	public static void main(String[] args)
+	{
+		Vacaciones vacacionesAlberto = new Vacaciones("Canarias",14,true);
+		
+		vacacionesAlberto.destino = "Canarias";
+		
+		vacacionesAlberto.mostrar();
+		
+	}
+
+}
