2848750c3c23c5d14435a4f38415d92750911c19
rodri <rodri@DESKTOP-IU8B4JD>
2021-09-30 09:18:05 +0200

    Rodri

--- BBDD_Patron_DAO/src/repositorios/ICRUD.java
+++ BBDD_Patron_DAO/src/repositorios/ICRUD.java
@@ -3,9 +3,11 @@ package repositorios;
 public interface ICRUD<Modelo,PK> {
 
 	void listar() throws Exception;
+	
 	Modelo leer(PK pk) throws Exception;
 	
 	void nuevo(Modelo modelo) throws Exception;
+	
 	void editar(PK pk) throws Exception;
 	
 	void eliminar(PK pk) throws Exception;;
